615e3a07c51a6e483e4ec04e1ddac4f938833da0
kanyesteves <kany.esteves@gmail.com>
2023-10-08 17:04:13 +0000

    Last updated - domingo - 08-10-23

--- examples/flags.sh
+++ examples/flags.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+#-----------------------CABEÇALHO---------------------------#
 
+#-------------------------FLAGS-----------------------------#
+
+#-----------------------VARIÁVEIS---------------------------#
 VERSION="1.0"
+PASSWORD="$(cat /etc/passwd | cut -d : -f 1 | sort)"
+HELP="
+  $(basename $0) - [ options ]
 
-echo "$VERSION"
+  -s - Ordem alfabética
+  -v - Versão
+  -h - Ajuda
+"
+#-----------------------EXECUÇÃO---------------------------#
+case "$1" in
+    -h) echo "$HELP"     && exit 0 ;;
+    -s) echo "$PASSWORD" && exit 0 ;;
+    -v) echo "$VERSION"  && exit 0 ;;
+     *) echo "$HELP"     && exit 0 ;;
+esac
